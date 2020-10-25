a85c29487be2b330e7353db7408759c20a09fc57
praneethpurnananda <praneeth.mandalemula@gmail.com>
2020-10-25 11:17:07 +0530

    added events module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { MainHomeComponent } from "./home/main-home/main-home.component";
 import { LoginComponent } from "./forms/login/login.component";
 import { RegisterComponent } from "./forms/register/register.component";
 import { ForgotPasswordComponent } from "./forms/forgot-password/forgot-password.component";
+import { AllEventsComponent } from "./events/all-events/all-events.component"
 
 const routes: Routes = [
   {path: '' , component: MainHomeComponent},
   {path: 'login' , component: LoginComponent},
   {path: 'register' , component: RegisterComponent},
-  {path: 'forgotpassword' , component: ForgotPasswordComponent}
+  {path: 'forgotpassword' , component: ForgotPasswordComponent},
+  {path: 'events' , component: AllEventsComponent}
 ];
 
 @NgModule({
