2c49864b764ae722cb481b9f36fab827a8065fae
sumalya <sumasunil1998@gmail.com>
2020-10-29 10:12:47 +0530

    new cardds

--- src/app/events/all-events/all-events.component.ts
+++ src/app/events/all-events/all-events.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-all-events',
   templateUrl: './all-events.component.html',
-  styleUrls: ['./all-events.component.css']
+  styleUrls: ['./all-events.component.css'], 
 })
 export class AllEventsComponent implements OnInit {
 
